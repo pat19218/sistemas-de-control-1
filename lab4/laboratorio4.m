4be2da5d31b0730720a34aef17d5239c4333009a
Cris Pat <68085020+pat19218@users.noreply.github.com>
2022-08-10 12:44:00 -0600

    6to

--- lab4/laboratorio4.m
+++ lab4/laboratorio4.m
@@ -7,7 +7,7 @@
 %% Primera Parte: Análisis y Modelado
 
 %% Segunda parte:Simulación y estudio del circuito
-caso = 4;
+caso = 6;
 
 switch caso
     case 1
@@ -27,17 +27,29 @@ switch caso
         %mientras mas pequenio c2, 
         %mas lejos del eje real
     case 4
-        %cuarto caso, más alejados del eje imaginario
+        %cuarto caso,Polos complejos conjugados más alejados del eje imaginario
         R1 =1e3; R2 =10e3;
         C1 = 10e-6; C2 = 0.20e-6;
         %mientras mas pequenio R1, 
         %mas lejos del eje imaginario
     case 5
-        %quinto caso, más alejados del eje imaginario
-        R1 =1e3; R2 =10e3;
+        %quinto caso,Polos complejos conjugados,más cercanos al eje imaginario
+        R1 =10e3; R2 =10e3;
         C1 = 10e-6; C2 = 0.20e-6;
-        %mientras mas pequenio R1, 
-        %mas lejos del eje imaginario
+        %mientras mas grande R1, 
+        %mas cerca del eje imaginario
+    case 6
+        %sexto caso,Polos reales iguales (aprox -15.64)
+        R1 = 6e3; R2 =6e3;
+        C1 = 10e-6; C2 = 10e-6;
+        %R1 y R2 deben ser iguales para tener tener multiplicidad 2 los
+        %polos
+    case 7
+        %septimo caso,Polos reales distintos
+        R1 = 30e3; R2 =30e3;
+        C1 = 10e-6; C2 = 10e-6;
+        %R1 y R2 deben ser iguales para tener tener multiplicidad 2 los
+        %polos
 end
 a = 1/(R1*R2*C1*C2);
 b = (R1+R2)/(R1*R2*C1);
